c1d9acdf6934831a2a1279f431bea49208504d41
Sadamodou <305965707@qq.com>
2016-09-08 13:19:37 +0800

    rename

---
+++ rename.ts
@@ -0,0 +1,17 @@
+﻿#! /usr/bin/env node
+let fs = require("fs");
+let path = require("path");
+
+function addNumber(filepath) {
+    fs.stat(filepath, function (err, stats) {
+        if (stats.isFile()) {
+            let filename = path.basename(filepath);
+            let parentDir = path.dirname(filepath);
+            let parentDirname = path.basename(path.dirname(filepath));
+            let thisFilename = path.basename(_filename);
+            if(filename!=thisFilename&&filename.indexof(parentDirname)<0){
+            
+            }
+        }
+    });
+}
